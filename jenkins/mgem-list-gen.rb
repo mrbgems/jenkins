d70c121341fe705e37dfb5aab3bfaf0f4c66ae6b
Takeshi Watanabe <takechi101010@gmail.com>
2017-12-04 00:31:11 +0900

    Update builder.

--- jenkins/mgem-list-gen.rb
+++ jenkins/mgem-list-gen.rb
@@ -32,19 +32,27 @@ cat <<EOF > "$WORKSPACE/mgem_build_config.rb"
     conf.toolchain tool
     enable_test if conf.respond_to? :enable_test
     enable_debug
+    conf.cxx.flags << '-std=c++11'
+    gem core: 'mruby-print'
     gem "${WORKSPACE}"
   end
 end
 EOF
 
 MRUBY_VERSIONS="1.3.0 1.2.0 master"
-for i in $MRUBY_VERSIONS; do
+
+res=0
+
+for ver in $MRUBY_VERSIONS; do
   cd $WORKSPACE
-  wget -O mruby-$i.tar.gz https://github.com/mruby/mruby/archive/$i.tar.gz
-  tar xf mruby-$i.tar.gz
-  cd mruby-$i
-  MRUBY_CONFIG="$WORKSPACE/mgem_build_config.rb" ./minirake all test
+  tar xf $WORKSPACE/../release-downloader/mruby-$ver.tar.gz
+  cd mruby-$ver
+  MRUBY_CONFIG="$WORKSPACE/mgem_build_config.rb" script -e -c  "./minirake all test" /dev/null || res=1
 done
+
+if [ "$res" == 1 ] ; then
+  exit 1
+fi
 EOS
   end
   File.open('./tmp.xml', 'w'){|f| config_xml.write(f) }
@@ -59,11 +67,10 @@ EOS
 end
 
 # mruby-masterの事後ビルド設定
-p `curl '#{base_url}/job/lang-mruby-master/config.xml'`
-mruby_master_xml = REXML::Document.new(`curl '#{base_url}/job/lang-mruby-master/config.xml'`)
+mruby_master_xml = REXML::Document.new(`curl '#{base_url}/job/release-downloader/config.xml'`)
 REXML::XPath.each(mruby_master_xml, '//hudson.tasks.BuildTrigger/childProjects') do |nd|
   nd.text = gem_names.join(', ')
 end
-File.open('./tmp.xml', 'w'){|f| mruby_master_xml.write(f) }
+File.open('./tmp.xml', 'w'){|f| mruby_master_xml.write(f) }	
 raise "cannot update mruby-master" unless
-  system("curl -X POST --data-binary @- -H #{crumb} -H 'Content-Type: text/xml' '#{base_url}/job/lang-mruby-master/config.xml' < ./tmp.xml")
+  system("curl -X POST --data-binary @- -H #{crumb} -H 'Content-Type: text/xml' '#{base_url}/job/release-downloader/config.xml' < ./tmp.xml")
